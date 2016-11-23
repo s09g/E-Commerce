0c7760a9a3d8709942fc1a916260860052d923d6
zhangshiqiu <zhangshiqiu1993@126.com>
2016-11-22 23:42:51 -0800

    price valid test

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -13,4 +10,21 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
   end
 
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+                          description: "yyy",
+                          image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+
+    product.price= 1
+    assert product.valid?
+  end
 end

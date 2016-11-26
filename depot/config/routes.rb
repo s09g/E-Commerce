9235465a85b103d12496e300fc3e229fe989db28
zhangshiqiu <zhangshiqiu1993@126.com>
2016-11-25 22:58:56 -0800

    front page of catalog

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'store/index'
+  # get 'store/index'
+  root 'store#index', as: 'store_index'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

358465074d70fe1988494475297eb3fa6f6bb26c
zhangshiqiu <zhangshiqiu1993@126.com>
2016-12-05 23:33:44 -0800

    fix a typo

--- depot/test/controllers/products_controller_test.rb
+++ depot/test/controllers/products_controller_test.rb
@@ -45,9 +45,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "can't delete product in cart" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
     end
+
+    assert_redirected_to products_url
   end
 
   test 'should destroy product' do

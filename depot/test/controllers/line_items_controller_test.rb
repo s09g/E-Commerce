3571e118840e9c1989cf18292c8556e8cb99ba02
zhangshiqiu <zhangshiqiu1993@126.com>
2016-12-06 15:14:14 -0800

    there is a bug in ruby sqlite3. Upgrading sqlite will fix that. use 'bundle update'

--- depot/test/controllers/line_items_controller_test.rb
+++ depot/test/controllers/line_items_controller_test.rb
@@ -5,40 +5,45 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     @line_item = line_items(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get line_items_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_line_item_url
     assert_response :success
   end
 
-  test "should create line_item" do
+  test 'should create line_item' do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
+    follow_redirect!
+
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', 'Programming Ruby 1.9'
+
+    # assert_redirected_to line_item_url(LineItem.last)
   end
 
-  test "should show line_item" do
+  test 'should show line_item' do
     get line_item_url(@line_item)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_line_item_url(@line_item)
     assert_response :success
   end
 
-  test "should update line_item" do
+  test 'should update line_item' do
     patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
-  test "should destroy line_item" do
+  test 'should destroy line_item' do
     assert_difference('LineItem.count', -1) do
       delete line_item_url(@line_item)
     end

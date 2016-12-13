4c1b628afeaf93473dd0732caa3a25c468f45f7c
zhangshiqiu <zhangshiqiu1993@126.com>
2016-12-13 00:35:49 -0800

    unfixable bug

--- depot/app/controllers/concerns/current_cart.rb
+++ depot/app/controllers/concerns/current_cart.rb
@@ -2,6 +2,8 @@ module CurrentCart
   private
 
     def set_cart
+      puts "Hello World\n\n\n\n\n"
+      puts session[:id]
       @cart = Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       @cart = Cart.create
--- depot/db/migrate/20161207203035_combine_items_in_cart.rb
+++ depot/db/migrate/20161207203035_combine_items_in_cart.rb
@@ -1,7 +1,4 @@
 class CombineItemsInCart < ActiveRecord::Migration[5.0]
-  def change
-  end
-
   def up
     Cart.all.each do |cart|
       sums = cart.line_items.group(:product_id).sum(:quantity)

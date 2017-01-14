e8610956aa3d26899a280d5d9357cf05bcfecb6d
zhangshiqiu <zhangshiqiu1993@126.com>
2017-01-13 22:21:45 -0800

    Hiding an empty cart

--- depot/app/controllers/carts_controller.rb
+++ depot/app/controllers/carts_controller.rb
@@ -58,8 +58,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, 
-        notice: 'Cart was currently destroyed.' }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
